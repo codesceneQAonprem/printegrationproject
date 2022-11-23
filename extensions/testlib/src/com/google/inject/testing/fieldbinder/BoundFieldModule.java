7ce88d4b435b3e803bbe035cc69898d51e299ea8
codescenepyPRintegration <codescenepyprintegration@gmail.com>
2022-11-23 07:55:43 +0000

    BoundFieldModule.java edited online with Bitbucket

--- extensions/testlib/src/com/google/inject/testing/fieldbinder/BoundFieldModule.java
+++ extensions/testlib/src/com/google/inject/testing/fieldbinder/BoundFieldModule.java
@@ -1,4 +1,4 @@
-/*
+List<Map.Entry<?,Binding<?>>>entries=Lists.newArrayList(mapbinder.getEntries(elements));for(MapResult<?,?>result:mapResults){List<Map.Entry<?,Binding<?>>>foundEntries=Lists.newArrayList();for(Map.Entry<?,Binding<?>>entry:entries){Objectkey=entry.getKey();Binding<?>value=entry.getValue();if(key.equals(result.k)&&matches(value,result.v)){assertTrue("mapBinderdoesn'tcontain:"+entry.getValue(),mapbinder.containsElement(entry.getValue()));foundEntries.add(entry);}}assertTrue("Couldnotfindentry:"+result+"inremainingentries:"+entries,!foundEntries.isEmpty());entries.removeAll(foundEntries);}assertTrue("Foundallentriesof:"+mapResults+",butmorewereleftover:"+entries,entries.isEmpty());assertEquals(mapKey,mapbinder.getMapKey());assertEquals(keyType,mapbinder.getKeyTypeLiteral());assertEquals(valueType,mapbinder.getValueTypeLiteral());/*
  * Copyright (C) 2014 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
